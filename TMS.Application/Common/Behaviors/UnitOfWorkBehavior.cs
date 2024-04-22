6ff4681b71371f6b7bc7b3f2251a5bc4face8c8f
AHMED <gammaltechstudent@gmail.com>
2024-04-22 05:14:46 +0200

    add : docker-compose.yml & UnitOfWorkBehavior.cs

---
+++ TMS.Application/Common/Behaviors/UnitOfWorkBehavior.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using TMS.Domain.Common.Repositories;
+
+namespace TMS.Application.Common.Behaviors;
+
+public class UnitOfWorkBehavior<TRequest,TResponse>: 
+    IPipelineBehavior<TRequest,TResponse> 
+    where TRequest : notnull 
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public UnitOfWorkBehavior(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if(IsNotCommand())
+        {
+            return await next();
+        }
+        var response = await next();
+        _unitOfWork.SaveChangesAsync(cancellationToken);
+        return response;
+    }
+
+    private static bool IsNotCommand()
+    {
+        return !typeof(TRequest).Name.EndsWith("Command");
+    }
+}
