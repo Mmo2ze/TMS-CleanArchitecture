b018496f5720b95de3e68d03dccfa1531fb38403
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-06-26 06:49:59 +0300

    fix : setSecondsToZero

--- TMS.Domain/Common/Extentions/TimeOnlyExtention.cs
+++ TMS.Domain/Common/Extentions/TimeOnlyExtention.cs
@@ -4,6 +4,6 @@ public static class TimeOnlyExtensions
 {
     public static TimeOnly SetSecondsToZero(this TimeOnly time)
     {
-        return new TimeOnly(time.Hour, time.Minute, time.Second, 0); // seconds and fraction-of-second are set to 0
+        return new TimeOnly(time.Hour, time.Minute,0); 
     }
 }
