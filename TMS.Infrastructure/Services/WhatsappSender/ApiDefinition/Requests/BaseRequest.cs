120a7fac97da0a30aa2d37e0dd24e130d301b758
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-08-14 23:08:57 +0300

    security : update WhatsappToken

--- TMS.Infrastructure/Services/WhatsappSender/ApiDefinition/Requests/BaseRequest.cs
+++ TMS.Infrastructure/Services/WhatsappSender/ApiDefinition/Requests/BaseRequest.cs
@@ -5,5 +5,5 @@ namespace TMS.Infrastructure.Services.WhatsappSender.ApiDefinition.Requests;
 public class BaseRequest
 {
 	[AliasAs("token")]
-	public string Token { get; init; } = "+dyKeJf/L7BNWtDwociSWbxw58LgdpHAZK5G2Lv6rsrh5ca/Ea+yArQtoiolrBnIb3YYqqpaIX+X0m0wnIrFxQ==";
+	public string Token { get; init; } = "JIRce4dfFw78JTcBqonJMCGvD2uNMWDb7UC0KIFu42w2Ltkg/6nvwWIwE/vJlqbC";
 }
