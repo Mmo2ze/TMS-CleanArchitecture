00675ca906fe985ee555d5a34ecfebe6809245e5
Ahmed Mohsen <99752390+Mmo2ze@users.noreply.github.com>
2024-08-14 23:09:35 +0300

    feat : add sortBy paying and Attendance Status in the request

--- TMS.Contracts/Account/Get/List/GetAccountsRequest.cs
+++ TMS.Contracts/Account/Get/List/GetAccountsRequest.cs
@@ -1,5 +1,13 @@
 using TMS.Application.Common;
+using TMS.Domain.Attendances;
 
 namespace TMS.Contracts.Account.Get.List;
 
-public record GetAccountsRequest(string? Search,string? GroupId,bool ?HasWhatsapp,int PageNumber = 1, int PageSize = 10) ;
+public record GetAccountsRequest(
+    string? Search,
+    string? GroupId,
+    bool? HasWhatsapp,
+    bool? IsPaid,
+    AttendanceStatus? AttendanceStatus,
+    int PageNumber = 1,
+    int PageSize = 10);
